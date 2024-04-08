3c3609c001d7689c327fe10bc7bf54d15af9438f
Hyuna Kim <19924___@naver.com>
2024-04-08 15:59:16 +1000

    initial commit

---
+++ pra.js
@@ -0,0 +1,25 @@
+// const target = 7;
+
+// const arr = [1, 4, 5, 3, 2];
+
+// let resultArr = [];
+// found = false;
+
+// for (let i = 0; i < arr.length && !found; i++) {
+//   for (let j = i + 1; j < arr.length; j++) {
+//     if (target - arr[i] - arr[j] === 0) {
+//       resultArr = [i, j];
+//       found = true;
+//       break;
+//     }
+//   }
+// }
+
+// console.log(resultArr);
+
+let a = 114;
+
+for (let i = 0; i < 20; i++) {
+  a = a * 2;
+  console.log(a);
+}
